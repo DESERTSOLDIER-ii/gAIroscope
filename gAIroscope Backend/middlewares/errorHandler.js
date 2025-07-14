8e68072947fb4e23dbdf9bd5d826bb74c9804ac3
DesertSoldier <Wealthymbungela02052000@gmail.com>
2025-07-14 13:00:24 +0200

    Delete gAIroscope Backend directory

--- gAIroscope Backend/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error("❌ Error:", err.stack || err);
-  res.status(500).json({
-    message: "Something went wrong!",
-    error: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-  });
-};
--- gAIroscope Backend/middlewares/requestLogger.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-  const time = new Date().toISOString();
-  console.log(`[${time}] ${req.method} ${req.originalUrl}`);
-  next();
-};
